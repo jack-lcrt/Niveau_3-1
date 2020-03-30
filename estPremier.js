e4be91dbe4b6ff93e561d9fe5fa2c537c1de2a35
Jacques Lacourt <jacques.lacourt@centrale-marseille.fr>
2020-03-30 12:04:24 +0200

    modifications

--- estPremier.js
+++ estPremier.js
@@ -1,24 +1,25 @@
-var estPremier = function(n){
-	if (n<0){return false;
-	} else {
-		for (var i = 2;n>i;i++){
-			if (n%i==0){
-				return (false);
-			}
-		}
-	return(true);
-	}
+const isPrime = function(n) {
+    if (n < 0) {
+        return false;
+    } else {
+        for (let i = 2; n > i; i++) {
+            if (n % i == 0) {
+                return (false);
+            }
+        }
+        return (true);
+    }
 };
 
-var listePremier = function(n){
-	var liste = [];
-	var nint=Math.floor(n);
-	for (var i=2;i<nint;i++){
-		if (estPremier(i)){
-			liste.push(i);
-		}
-	}
-	return (liste);
+const listPrime = function(n) {
+    let list = [];
+    const nint = Math.floor(n);
+    for (let i = 2; i < nint; i++) {
+        if (isPrime(i)) {
+            list.push(i);
+        }
+    }
+    return (list);
 };
 
-module.exports = listePremier;
+module.exports = listPrime;
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express=require("express");
-const app=express();
+const express = require("express");
+const app = express();
 const bodyParser = require("body-parser");
-const listePremier=require("./estPremier");
+const listPrime = require("./estPremier");
 
 app.use(bodyParser.json());
 
-app.get("/api",(req,res) => {
-	var nb= req.body.nombre;
-	if (nb>0){
-	res.send({'racine carré:':Math.sqrt(nb),
-'liste des nombres premiers:':listePremier(nb)});
+app.get("/api", (req, res) => {
+    var nb = req.body.nombre;
+    if (nb > 0) {
+        res.send({
+            'square root:': Math.sqrt(nb),
+            'list of prime numbers:': listPrime(nb)
+        });
 
-}
-	
+    }
+
 });
 
 app.listen(4000);
